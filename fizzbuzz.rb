31fd6cab66c9ce7dfdc8ea210b1d25cc1882aae7
Milan Mody <milanm1120@gmail.com>
2020-12-12 03:38:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,7 @@
 # See README.md for instructions on how to do this
 
 require_relative './spec_helper.rb'
+require_relative '../fizzbuzz.rb'
 
 def fizzbuzz(int)
   if int % 3 == 0
@@ -12,4 +13,5 @@ def fizzbuzz(int)
     puts "FizzBuzz"
   else int % 3 || int % 5 != 0
     puts nil
+  end
 end
